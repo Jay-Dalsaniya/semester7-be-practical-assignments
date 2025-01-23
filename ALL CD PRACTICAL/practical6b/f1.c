a3911b98114849727ec66cb272cdf7539695d92d
Jay-Dalsaniya <168808927+Jay-Dalsaniya@users.noreply.github.com>
2025-01-23 18:54:34 +0530

    All Cd Practical

---
+++ ALL CD PRACTICAL/practical6b/f1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    /*
+     * This program is a basic example of how to use
+     * comments in C. The alignment of asterisks (*) 
+     * makes it easier to read and maintain.
+     */
+
+    // This is the start of the program execution
+    /*
+     This is a B-Batch 7tc4 students lab.
+     */
+    // Print a simple greeting message to the console
+    printf("Hello, World!\n");
+
+    /*
+     * The return value is 0, which signals that
+     * the program executed without any errors.
+     */
+
+    // End of the main function, return 0 indicates success
+    return 0;
+}
